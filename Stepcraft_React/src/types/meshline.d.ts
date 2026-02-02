57f0bf0c9f8e104bb170d621b2d20633a9e7b063
JaggerBean <jagger@utexas.edu>
2026-02-02 12:54:03 -0500

    I switched the typings to the proper @react-three/fiber module augmentation (the previous global JSX declaration wasn’t being picked up by the bundler).

--- Stepcraft_React/src/types/meshline.d.ts
+++ Stepcraft_React/src/types/meshline.d.ts
@@ -1,10 +1,9 @@
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      meshLineGeometry: any;
-      meshLineMaterial: any;
-    }
+import type { MeshLineGeometry, MeshLineMaterial } from "meshline";
+import type { Object3DNode } from "@react-three/fiber";
+
+declare module "@react-three/fiber" {
+  interface ThreeElements {
+    meshLineGeometry: Object3DNode<MeshLineGeometry, typeof MeshLineGeometry>;
+    meshLineMaterial: Object3DNode<MeshLineMaterial, typeof MeshLineMaterial>;
   }
 }
-
-export {};
