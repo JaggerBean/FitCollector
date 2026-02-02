ce8488939712b99604f0b6207e6822daedf4045c
JaggerBean <jagger@utexas.edu>
2026-02-02 12:51:53 -0500

    fixed proper TS declarations

---
+++ Stepcraft_React/src/vite-env.d.ts
@@ -0,0 +1,18 @@
+/// <reference types="vite/client" />
+
+declare module "*.glb" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.png" {
+  const src: string;
+  export default src;
+}
+
+declare namespace JSX {
+  interface IntrinsicElements {
+    meshLineGeometry: any;
+    meshLineMaterial: any;
+  }
+}
