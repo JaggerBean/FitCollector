9918fac95f154df2b680a32108ea891ef9b12247
JaggerBean <jagger@utexas.edu>
2026-02-06 13:02:14 -0500

    button gradient change

--- Stepcraft_React/src/components/StarBorder.tsx
+++ Stepcraft_React/src/components/StarBorder.tsx
@@ -43,7 +43,7 @@ const StarBorder = <T extends React.ElementType = "button">({
           animationDuration: speed,
         }}
       ></div>
-      <div className="relative z-[1] bg-gradient-to-b from-black to-gray-900 border border-gray-800 text-white text-center text-[16px] py-[16px] px-[26px] rounded-[20px]">
+      <div className="relative z-[1] bg-gradient-to-b from-emerald-950/90 via-slate-950 to-slate-950 border border-emerald-900/70 text-white text-center text-[16px] py-[16px] px-[26px] rounded-[20px]">
         {children}
       </div>
     </Component>
