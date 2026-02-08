a55f69bc9e05a4f5d6d2029f5963bbd04ab8453c
JaggerBean <jagger@utexas.edu>
2026-02-08 11:45:28 -0500

    better carousel delay

--- Stepcraft_React/src/components/Carousel.tsx
+++ Stepcraft_React/src/components/Carousel.tsx
@@ -199,7 +199,7 @@ export default function Carousel({
   minItemWidth = 360,
   visibleCount,
   autoplay = false,
-  autoplayDelay = 4500,
+  autoplayDelay = 8000,
   pauseOnHover = false,
   loop = false,
   round = false,
